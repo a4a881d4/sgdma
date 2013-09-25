fbb54aca144ec0354ca8102816ddd0af55c19016
a4a881d4 <a4a881d4@163.com>
2013-09-25 21:03:08 +0800

    remove

--- .sgdma.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/dx/works/altera/code/sgdma/sgdma.o := ld -m elf_x86_64   -r -o /home/dx/works/altera/code/sgdma/sgdma.o /home/dx/works/altera/code/sgdma/usgproc.o /home/dx/works/altera/code/sgdma/usgdma.o 
--- .tmp_versions/sgdma.mod
+++
@@ -1,2 +0,0 @@
-/home/dx/works/altera/code/sgdma/sgdma.ko
-/home/dx/works/altera/code/sgdma/usgproc.o /home/dx/works/altera/code/sgdma/usgdma.o
